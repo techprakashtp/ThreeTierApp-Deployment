dd7c427d6087524c461b85b2f988ed7cba6fd765
techprakashtp <jyotiprakashnk49@gmail.com>
2024-05-03 23:43:19 +0530

    Update k8s deployment manifest file Image to version

---
+++ K8S-Manifest/Frontend@tmp/durable-dab4a860/script.sh
@@ -0,0 +1,16 @@
+
+                    git config user.name "techprakashtp"
+		    git config user.email "jyotiprakashnk49@gmail.com"  
+
+		    git config clean.requireForce false
+		    git clean -f
+		   
+		    sed -i 's/todoapp-frontend/todoapp-frontend:${BASE_VERSION}/g' deployment.yaml
+                    
+		    cat deployment.yaml 
+		    git add -A
+		    
+                    git commit -m "Update k8s deployment manifest file Image to version ${fullVersion}"
+                    git push https://${GITHUB_CRED}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME} HEAD:ops-branch1
+
+        	     
